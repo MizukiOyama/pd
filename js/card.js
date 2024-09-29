0d836ffe152f9340c44f9f49d2c07781fcd5e0da
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-09-29 18:26:37 +0900

    acc se list tab crad crad js

--- js/card.js
+++ js/card.js
@@ -1,46 +1,46 @@
 const cardModalContents = [
     {
-        left: "Web Design",
-        right: "<p>HP、LPの制作を行います。</p>"
+        left: "Web Design<p>HP、LPの制作を行います。</p>",
+        right: "<p>責任を持って最後まで提供するものにこだわり制作いたします。※制作を検討中の方でも問合せを受け付けているので、お気軽にお問い合わせください。</p>"
     },
     {
-        left: "Art Order",
-        right: "<p>Abstract Art、Digital Art、などの制作が可能です。</p>"
+        left: "Art Order<p>Art作品の制作を行います。</p>",
+        right: "<p>Abstract Art、Digital Art、などの制作が可能です。新築、新居にも合うArt作品を提供いたします。</p>"
     },
     {
         left: "Consulting",
         right: "<p>新規事業の立ち上げへのアドバイスや事業の土台固めを行います。</p>"
     },
     {
-        left: "illustration",
+        left: "Illustration<p>様々な用途で使用できるイラストの制作を行います。</p>",
         right: "<p>ウェブ、印刷物、イメージキャラクター、などのイラストを制作します。</p>"
     },
     {
-        left: "writing",
+        left: "Writing<p></p>",
         right: "<p>ウェブ、印刷物、などのライティングを行います。</p>"
     },
     {
-        left: "photo",
+        left: "Photo<p></p>",
         right: "<p>ウェブ、ポートレート、商品、などの写真撮影を行います。</p>"
     },
     {
-        left: "video",
+        left: "Video<p></p>",
         right: "<p>ウェブ、などで使用する映像を撮影・制作を行います。</p>"
     },
     {
-        left: "DTP",
+        left: "DTP<p></p>",
         right: "<p>印刷物の制作を行います。「メニュー表、名刺、チラシ、ポスター」etc...</p>"
     },
     {
-        left: "banner",
+        left: "Banner<p></p>",
         right: "<p>広告用のバナー制作を行います。</p>"
     },
     {
-        left: "logo",
+        left: "Logo<p></p>",
         right: "<p>会社や組織団体のオリジナルのロゴを制作します。</p>"
     },
     {
-        left: "その他",
+        left: "etc...<p></p>",
         right: "<p>表記のないものを依頼したい場合は、Contactページからお問合せください。</p>"
     }
 ];
