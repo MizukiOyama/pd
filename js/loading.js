fde7fcb98de58b862cd7164e89b75169a79e64e5
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-10-01 20:40:59 +0900

    loading

--- js/loading.js
+++ js/loading.js
@@ -5,32 +5,32 @@ jQuery(document).ready(function($) {
     const $soundToggle = $('#sound-toggle');
     const $music = $('#background-music');
     
-    // 1. 初回訪問時のみローディング画面を表示
+    // 初回訪問時のみローディング画面を表示
     if (firstVisit) {
         sessionStorage.setItem('visited', true);
 
-        // 2. ローディング画面フェードイン
+        // ローディング画面フェードイン
         $loadingBg.addClass('show');
 
-        // 3. テキスト表示とシャッフルアニメーション
+        // テキスト表示とシャッフルアニメーション
         setTimeout(function() {
             $typing.addClass('endAnime');
             const shuffleText = new ShuffleText($typing[0]);
             shuffleText.setText($typing.text());
             shuffleText.start();
-        }, 500); // ロード後にテキスト表示
+        }, 500);
 
-        // 4. テキストフェードアウト
+        // テキストフェードアウト
         setTimeout(function() {
             $typing.removeClass('endAnime').fadeOut(1000);
-        }, 3000); // 3秒後にテキストをフェードアウト
+        }, 3000);
 
-        // 5. サウンドオン/オフボタンの表示
+        // サウンドオン/オフボタンの表示
         setTimeout(function() {
             $soundToggle.fadeIn(1000);
-        }, 4000); // テキストがフェードアウトした後にサウンドボタン表示
+        }, 4000); 
 
-        // 6. サウンドのオン/オフ切り替え
+        // サウンドのオン/オフ切り替え
         $('#sound-on').on('click', function() {
             $music[0].play();
             finishLoading();
@@ -41,15 +41,15 @@ jQuery(document).ready(function($) {
             finishLoading();
         });
 
-        // 7. ローディング画面フェードアウト
+        // ローディング画面フェードアウト
         function finishLoading() {
-            $soundToggle.fadeOut(1000); // ボタンを非表示にする
+            $soundToggle.fadeOut(1000); 
             setTimeout(function() {
                 $loadingBg.removeClass('show');
-            }, 1000); // 画面をフェードアウト
+            }, 1000); 
         }
     } else {
-        // 再読み込み時にはローディング画面を表示しない
+        // 再読み込み時にはローディング画面をスキップ
         $loadingBg.removeClass('show');
     }
 });
