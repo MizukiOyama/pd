606a374ebfe3993ac2339776040f13eec804acb7
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-10-01 20:35:08 +0900

    loading

--- js/loading.js
+++ js/loading.js
@@ -1,50 +1,55 @@
-// ShuffleText クラスの定義
-class ShuffleText {
-    constructor(element) {
-        this.element = element;
-        this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        this.originalText = element.innerHTML;
-    }
-
-    setText(text) {
-        this.originalText = text;
-    }
-
-    randomChar() {
-        return this.chars[Math.floor(Math.random() * this.chars.length)];
-    }
-
-    shuffle() {
-        let shuffledText = '';
-        for (let i = 0; i < this.originalText.length; i++) {
-            shuffledText += this.randomChar();
-        }
-        this.element.innerHTML = shuffledText;
-    }
-
-    start() {
-        const shuffleInterval = setInterval(() => {
-            this.shuffle();
-        }, 100);
-
-        setTimeout(() => {
-            clearInterval(shuffleInterval);
-            this.element.innerHTML = this.originalText;
-        }, 2000);
-    }
-}
-
 jQuery(document).ready(function($) {
-    // ローディング終了後にサウンドボタンの選択を促す
-    $('#sound-toggle').show();
+    let firstVisit = !sessionStorage.getItem('visited');
+    const $loadingBg = $('#loading-bg');
+    const $typing = $('.js_typing');
+    const $soundToggle = $('#sound-toggle');
+    const $music = $('#background-music');
+    
+    // 1. 初回訪問時のみローディング画面を表示
+    if (firstVisit) {
+        sessionStorage.setItem('visited', true);
 
-    // ページの読み込み後にローディング背景を非表示にする
-    $(window).on('load', function() {
+        // 2. ローディング画面フェードイン
+        $loadingBg.addClass('show');
+
+        // 3. テキスト表示とシャッフルアニメーション
         setTimeout(function() {
-            $(".js_typing").each(function() {
-                var shuffleText = new ShuffleText(this);
-                shuffleText.start();
-            });
-        }, 500);  // テキストアニメーション開始
-    });
+            $typing.addClass('endAnime');
+            const shuffleText = new ShuffleText($typing[0]);
+            shuffleText.setText($typing.text());
+            shuffleText.start();
+        }, 500); // ロード後にテキスト表示
+
+        // 4. テキストフェードアウト
+        setTimeout(function() {
+            $typing.removeClass('endAnime').fadeOut(1000);
+        }, 3000); // 3秒後にテキストをフェードアウト
+
+        // 5. サウンドオン/オフボタンの表示
+        setTimeout(function() {
+            $soundToggle.fadeIn(1000);
+        }, 4000); // テキストがフェードアウトした後にサウンドボタン表示
+
+        // 6. サウンドのオン/オフ切り替え
+        $('#sound-on').on('click', function() {
+            $music[0].play();
+            finishLoading();
+        });
+
+        $('#sound-off').on('click', function() {
+            $music[0].pause();
+            finishLoading();
+        });
+
+        // 7. ローディング画面フェードアウト
+        function finishLoading() {
+            $soundToggle.fadeOut(1000); // ボタンを非表示にする
+            setTimeout(function() {
+                $loadingBg.removeClass('show');
+            }, 1000); // 画面をフェードアウト
+        }
+    } else {
+        // 再読み込み時にはローディング画面を表示しない
+        $loadingBg.removeClass('show');
+    }
 });
