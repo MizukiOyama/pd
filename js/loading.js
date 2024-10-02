6e8986be1236bd920524e00fa5410b20a65276ff
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-10-02 11:31:36 +0900

    loading

--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,38 @@
+class ShuffleText {
+    constructor(element) {
+        this.element = element;
+        this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        this.originalText = element.innerHTML;
+    }
+
+    setText(text) {
+        this.originalText = text;
+    }
+
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+
+    shuffle() {
+        let shuffledText = '';
+        for (let i = 0; i < this.originalText.length; i++) {
+            shuffledText += this.randomChar();
+        }
+        this.element.innerHTML = shuffledText;
+    }
+
+    start() {
+        const shuffleInterval = setInterval(() => {
+            this.shuffle();
+        }, 100);
+
+        setTimeout(() => {
+            clearInterval(shuffleInterval);
+            this.element.innerHTML = this.originalText;
+        }, 2000); // テキストが元に戻るまでの時間
+    }
+}
+
 jQuery(document).ready(function($) {
     const $loadingBg = $('#loading-bg');
     const $typing = $('.js_typing');
