aadb3c071fea91b064b68c2831d63fdac87a2645
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-10-02 12:07:04 +0900

    loading

--- js/loading.js
+++ js/loading.js
@@ -1,38 +1,3 @@
-class ShuffleText {
-    constructor(element) {
-        this.element = element;
-        this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        this.originalText = element.innerHTML;
-    }
-
-    setText(text) {
-        this.originalText = text;
-    }
-
-    randomChar() {
-        return this.chars[Math.floor(Math.random() * this.chars.length)];
-    }
-
-    shuffle() {
-        let shuffledText = '';
-        for (let i = 0; i < this.originalText.length; i++) {
-            shuffledText += this.randomChar();
-        }
-        this.element.innerHTML = shuffledText;
-    }
-
-    start() {
-        const shuffleInterval = setInterval(() => {
-            this.shuffle();
-        }, 100);
-
-        setTimeout(() => {
-            clearInterval(shuffleInterval);
-            this.element.innerHTML = this.originalText;
-        }, 2000); // テキストが元に戻るまでの時間
-    }
-}
-
 jQuery(document).ready(function($) {
     const $loadingBg = $('#loading-bg');
     const $typing = $('.js_typing');
@@ -60,51 +25,15 @@ jQuery(document).ready(function($) {
         shuffleText.start();
     }
 
-    // サウンドオプションを表示
-    function showSoundOptions() {
-        $soundToggle.css({ display: 'flex', visibility: 'visible', opacity: 1 }); // フェードイン
-    }
-
-    // サウンドオプションをフェードアウトして非表示
-    function hideSoundOptions() {
-        $soundToggle.css({ opacity: 0 });
-        setTimeout(() => {
-            $soundToggle.css({ visibility: 'hidden' });
-        }, 1000);
-    }
-
-    // サウンドのオン・オフ切り替え
-    $('#sound-on').on('click', function() {
-        $music[0].play();
-        finishLoading();
-    });
-
-    $('#sound-off').on('click', function() {
-        $music[0].pause();
-        finishLoading();
-    });
-
-    // ローディング画面終了処理
-    function finishLoading() {
-        hideSoundOptions(); // サウンドオプションをフェードアウト
-        setTimeout(hideLoadingScreen, 1000); // サウンドオプションが消えた後にローディング画面をフェードアウト
-    }
-
     // 初回訪問時のみローディング画面を表示
     if (!sessionStorage.getItem('visited')) {
         sessionStorage.setItem('visited', true);
 
-        // ローディング画面表示
+        // ローディング画面を表示
         showLoadingScreen();
 
         // テキスト表示とアニメーション
         setTimeout(startTextAnimation, 500);
-
-        // テキストフェードアウトとサウンドオプション表示
-        setTimeout(() => {
-            $typing.removeClass('endAnime').fadeOut(1000);
-            showSoundOptions();
-        }, 3000);
 
     } else {
         // 再訪問時はローディング画面をスキップ
