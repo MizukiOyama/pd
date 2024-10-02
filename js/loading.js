4e4a781dee023d0f9b8f8a6f30ca0b0d74e36bbb
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-10-02 13:01:04 +0900

    loading

--- js/loading.js
+++ js/loading.js
@@ -1,24 +1,48 @@
+class ShuffleText {
+    constructor(element) {
+        this.element = element;
+        this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        this.originalText = element.innerHTML;
+    }
+
+    setText(text) {
+        this.originalText = text;
+    }
+
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+
+    shuffle() {
+        let shuffledText = '';
+        for (let i = 0; i < this.originalText.length; i++) {
+            shuffledText += this.randomChar();
+        }
+        this.element.innerHTML = shuffledText;
+    }
+
+    start() {
+        const shuffleInterval = setInterval(() => {
+            this.shuffle();
+        }, 100);
+
+        // シャッフルを1秒後に停止
+        setTimeout(() => {
+            clearInterval(shuffleInterval);
+            this.element.innerHTML = this.originalText; // 元のテキストを設定
+            $(this.element).addClass('endAnime'); // アニメーションが終了したことを示すクラスを追加
+            setTimeout(() => {
+                $('#loading-bg').fadeOut(); // ローディング画面をフェードアウト
+            }, 500); // テキスト表示後0.5秒後にローディング画面をフェードアウト
+        }, 1000); // シャッフルを1秒後に停止
+    }
+}
+
 jQuery(document).ready(function($) {
-    const $loadingBg = $('#loading-bg');
-    const $typing = $('.js_typing');
+    // ローディング画面を表示する
+    $('#loading-bg').css({ visibility: 'visible', opacity: 1 });
 
-    console.log('Loading started'); // ログで動作確認
-
-    function showLoadingScreen() {
-        console.log('Showing loading screen'); // ログで動作確認
-        $loadingBg.css({ visibility: 'visible', opacity: 1 });
-    }
-
-    // 初回訪問時のみローディング画面を表示
-    if (!sessionStorage.getItem('visited')) {
-        sessionStorage.setItem('visited', true);
-
-        // ローディング画面を表示
-        showLoadingScreen();
-
-        // テキスト表示とアニメーション
-        setTimeout(startTextAnimation, 500);
-    } else {
-        $loadingBg.css({ opacity: 0, visibility: 'hidden' });
-    }
+    // テキストのシャッフルアニメーションを開始
+    const shuffleText = new ShuffleText($('.js_typing')[0]);
+    shuffleText.start();
 });
