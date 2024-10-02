7d1ea85a61b5aa1f9ac5593d6a161862c872bce0
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-10-02 12:23:48 +0900

    load js

--- js/loading.js
+++ js/loading.js
@@ -24,50 +24,12 @@ class ShuffleText {
     start() {
         const shuffleInterval = setInterval(() => {
             this.shuffle();
-        }, 100);
+        }, 100); // 100ミリ秒ごとにシャッフル
 
+        // 1秒後にシャッフルを停止
         setTimeout(() => {
             clearInterval(shuffleInterval);
-            this.element.innerHTML = this.originalText;
-        }, 2000);
+            this.element.innerHTML = this.originalText; // 元のテキストに戻す
+        }, 1000); // 1秒（1000ミリ秒）で終了
     }
 }
-
-jQuery(document).ready(function($) {
-    // ページの読み込み後にローディング背景を非表示にする
-    $(window).on('load', function() {
-        setTimeout(function() {
-            $('#loading-bg').fadeOut();
-        }, 1500);
-    });
-
-    // テキストのアニメーション効果を実装する関数
-    function TypingAnimation() {
-        $(".js_typing").each(function(i) {
-            var elemPos = $(this).offset().top - 50; // 要素より、50px上の位置
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll >= elemPos - windowHeight) {
-                if (!$(this).hasClass("endAnime")) {
-                    $(this).addClass("endAnime"); // アニメーションが終了したことを示すクラスを追加
-                    var text = $(this).text(); // テキストを取得
-                    $(this).text(''); // テキストを空にする
-                    var shuffleText = new ShuffleText(this); // ShuffleTextのインスタンスを生成
-                    $(this).append(shuffleText); // ShuffleTextを要素に追加
-                    shuffleText.setText(text); // テキストを設定
-                    shuffleText.start(); // アニメーションを開始
-                }
-            }
-        });
-    }
-
-    // 画面をスクロールしたときのイベント
-    $(window).scroll(function() {
-        TypingAnimation(); // テキストのアニメーション効果を実行
-    });
-
-    // ページが読み込まれたときのイベント
-    $(window).on("load", function() {
-        TypingAnimation(); // テキストのアニメーション効果を実行
-    });
-});
