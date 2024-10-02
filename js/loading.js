371c05f4461b8b77803c4c44d7ebfadee42a58e1
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-10-02 17:46:37 +0900

    load

--- js/loading.js
+++ js/loading.js
@@ -5,10 +5,6 @@ class ShuffleText {
         this.originalText = element.innerHTML;
     }
 
-    setText(text) {
-        this.originalText = text;
-    }
-
     randomChar() {
         return this.chars[Math.floor(Math.random() * this.chars.length)];
     }
@@ -26,35 +22,26 @@ class ShuffleText {
             this.shuffle();
         }, 100);
 
-        // シャッフルを2.5秒後に停止し、元のテキストを表示
+        // 1秒後にシャッフルを停止して元のテキストを表示
         setTimeout(() => {
             clearInterval(shuffleInterval);
             this.element.innerHTML = this.originalText;
-            
-            // シャッフル終了後、オン・オフボタンを表示
-            $('#sound-options').fadeIn(500); // 0.5秒かけてボタン表示
-        }, 2500);
+        }, 3500);
     }
 }
 
 jQuery(document).ready(function($) {
-    // ページ読み込み後にテキストシャッフル開始
+    // ページの読み込み後にテキストシャッフルを開始
     $(window).on('load', function() {
-        const typingElement = $('.js_typing'); // 要素を取得
+        const typingElement = $('.js_typing');
         if (typingElement.length > 0) {
             const shuffleText = new ShuffleText(typingElement[0]);
             shuffleText.start();
         }
-    });
 
-    // オン・オフボタンのクリックイベント処理
-    $('#sound-on').on('click', function() {
-        console.log('Sound On selected');
-        $('#loading-bg').fadeOut(1000); // 1秒かけてローディング画面をフェードアウト
-    });
-
-    $('#sound-off').on('click', function() {
-        console.log('Sound Off selected');
-        $('#loading-bg').fadeOut(1000); // 1秒かけてローディング画面をフェードアウト
+        // 2.5秒後にローディング画面をフェードアウト
+        setTimeout(function() {
+            $('#loading-bg').fadeOut();
+        }, 3500);
     });
 });
