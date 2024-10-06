2ae1d0a97115d876ea9fdff23aa7f26a02a35c14
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-10-06 12:41:12 +0900

    audio

--- js/menu.js
+++ js/menu.js
@@ -81,28 +81,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    // 要素の取得
-    const toggleSwitch = document.getElementById('soundToggle');
-    const backgroundAudio = document.getElementById('backgroundAudio');
-
-    // 要素が正しく取得できたか確認
-    if (toggleSwitch && backgroundAudio) {
-        // チェックボックスの状態に応じてサウンドを制御
-        toggleSwitch.addEventListener('change', function () {
-            if (toggleSwitch.checked) {
-                console.log('Audio playing...');
-                backgroundAudio.play();
-            } else {
-                console.log('Audio stopped...');
-                backgroundAudio.pause();
-                backgroundAudio.currentTime = 0; // 再生位置をリセット
-            }
-        });
-    } else {
-        // 要素が取得できなかった場合のエラーメッセージ
-        console.error('Toggle switch or background audio element not found');
-    }
-});
-
