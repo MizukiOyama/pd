5409e95491f3d0a310febeb8ce4166b9ace82e31
m.oyama <154662763+MizukiOyama@users.noreply.github.com>
2024-10-24 15:20:44 +0900

    モーダル js

--- js/card.js
+++ js/card.js
@@ -3,7 +3,7 @@ const cardModalContents = [
         left: "<h4>Web Design</h4><p>基本的なHTML/CSS/JavaScriptを使用してコーディングを行い制作いたします。「TOP、About、Service、Contact、Information」などの1～5ページ程度のシンプルなサイト制作を行います。 ビジュアルはもちろん、文章に乗せる言葉などの細部もこだわり制作いたします。<br>※制作を検討中の方でも問合せを受け付けているので、お気軽にお問い合わせください。アニメーション、表示速度の調整も可能です。</p>"
     },
     {
-        left: "<h4>Art Order</h4><p>Art作品の制作を行います。<br>Abstract Art、Digital Art、などの制作が可能です。 新築、新居にも合うArt作品を提供いたします。<br>※サイズや使用する画材で価格が変動するので、Orderをご検討の方は問合せにてOrderの詳細をお伝えいたしますので、お気軽にお申し付けください。</p>"
+        left: "<h4>Art Order</h4><p>Art作品の制作を行います。Abstract Art、Digital Art、などの制作が可能です。新築、新居にも合うArt作品を提供いたします。<br>※サイズや使用する画材で価格が変動するので、Orderをご検討の方は問合せにてOrderの詳細をお伝えいたしますので、お気軽にお申し付けください。</p>"
     },
     {
         left: "<h4>Consulting</h4><p>個人事業や新規事業の立ち上げへのアドバイスや事業の土台固めを行います。<br>※無料相談「1時間」のお申込みもできますのでお気軽にお申し付けください。※個人事業や新規事業の土台固め、ブランディングの補助を行いますので、ビジョンやこだわり、想いなどの情報共有をお願いいたします。</p>"
@@ -12,7 +12,7 @@ const cardModalContents = [
         left: "<h4>Illustration</h4><p>Webやチラシなど様々な用途で使用できる雰囲気に合ったイラストを提供します。対応しているもの、「ウェブ、印刷、イメージキャラクター」などのイラストを制作します。</p>"
     },
     {
-        left: "<h4>Writing</h4><p>ウェブ、印刷物、などで使用する文章のライティングを行います。「キャッチコピー、SEOを意識した文章」など。<br>※お申込み後の面談前日までに乗せたい「文章、キャッチコピー」などをご用意ください。</p>"
+        left: "<h4>Writing</h4><p>ウェブ、印刷物、などで使用する文章のライティングを行います。「キャッチコピー、SEOを意識した文章」などのライティングを行います。<br>※お申込み後の面談前日までに乗せたい「文章、キャッチコピー」などがございましたら共有をお願いいたします。</p>"
     },
     {
         left: "<h4>Photo</h4><p>Webで使用する写真や商品や個人のプロフィール写真撮影。シンプルなセットで、手軽な撮影を行います。<br>※遠方の撮影も可能ですが、別途交通費のご負担をお願いしています。</p>"
@@ -108,7 +108,7 @@ const cardModalContents = [
     modal.style.width = "85vmin";
     modal.style.height = "55%";
     modal.style.transform = "translate(-50%, -50%)";
-    modal.style.backgroundColor = "rgba(46, 46, 46, 0.5)";
+    modal.style.backgroundColor = "rgba(46, 46, 46, 0.35)";
     modal.style.zIndex = "1000";
     modal.style.padding = "30px";
     modal.style.boxShadow = "rgba(0, 0, 0, 0.6) 0px 2px 3px";
